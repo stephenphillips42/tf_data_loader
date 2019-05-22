6e36281bc0a9aca2711ff2a92b17edf2e3e32889
Stephen Phillips <chengdu.scp@gmail.com>
2019-05-22 14:02:04 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='tf_data_loader',
+      version='0.1',
+      description='Data loader for many data types for Tensorflow',
+      url='https://github.com/stephenphillips42/tf_data_loader',
+      author='Stephen Phillips',
+      author_email='stephi@seas.upenn.edu',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
